d45a98adbc525e47e1d8def680b63070c89977e2
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2022-01-18 22:42:18 +0000

    Did a little programming

--- customNueralNetThing.py
+++ customNueralNetThing.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 class node:
     def __init__(self, Layer, Num):
@@ -8,13 +9,14 @@ class node:
 
 
 class connection:
-    def __init__(self, Layer, A, B, Init_Value, maxweight):
+    def __init__(self, Layer, A, B, Init_Value, maxweight, bias):
         self.Layer = Layer
         self.NodeA = A
         self.NodeB = B
         self.value = Init_Value
         self.weight = 1
         self.maxWeight = maxweight
+        self.bias = 0
 
     def send(self):
         self.NodeB.value = self.NodeA.value * self.value
@@ -61,14 +63,35 @@ class Handler:
         createTime = time.time() - startTime
         print(f'Network took {createTime} seconds to create')
 
+    def Calculate(self, inputs: list):
+        tmp = 0
+        for x in self.connections:
+            for y in x:
+                tmp += len(y)
+        print(tmp)
+
+        # if (len(inputs) == len(self.FrontLayerNodes)):
+        #     for layer in self.connections:
+        #         self.CalcLayer(layer)
+        # else:
+        #     print(f'Input size of {len(inputs)} is not equal to the required {len(self.FrontLayerNodes)} needed.')
+
+    def CalcLayer(self, Layerconnections: list):
+        for connection in Layerconnections:
+            tmpSum = 0
+                
+
+    def Sigmoid(value):
+        return (1/(1+math.exp(-value)))
+
     def connectTwoNodes(self, NodeA: node, NodeB: node, Init_Value=0.5, maxWeight=100):
         # This funky wunky function connects two Nodes by creating a connection object between the two
-        NewConnection = connection(NodeA.Layer, NodeA, NodeB, Init_Value, maxWeight)
+        NewConnection = connection(NodeA.Layer, NodeA, NodeB, Init_Value, maxWeight, 0)
         return NewConnection
 
     def connectNodeToNextLayer(self, NodeA: node):
         # Connect a Node to all nodes in the next layer
-        NextNodes = self.getNodesinLayer(NodeA.Layer)
+        NextNodes = self.getNodesinLayer(NodeA.Layer + 1)
         NewConnections = []
         for node in NextNodes:
             NewConnections.append(self.connectTwoNodes(NodeA, node))
@@ -84,7 +107,7 @@ class Handler:
         self.connections.append(tmp)
 
     def createConnections(self):
-        for layer in range(self.LayerCount - 1):
+        for layer in range(self.LayerCount - 2): # subtract 1 for indexing, one because there should be 1 less connection layer than node layers
             self.connectLayerToNextLayer(layer)
 
     def getNodesinLayer(self, Layer):
@@ -104,4 +127,5 @@ class Handler:
             return None
 
 
-Network = Handler(5, 784, 10, 6)
+Network = Handler(4, 784, 10, 16)
+Network.Calculate([0])
