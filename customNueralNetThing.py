c2ffe38fec826744f52578b66ce7e4422fee575d
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2022-01-19 10:08:56 +0000

    Holy sheeesh it workey :0

--- customNueralNetThing.py
+++ customNueralNetThing.py
@@ -2,21 +2,21 @@ import time
 import math
 
 class node:
-    def __init__(self, Layer, Num):
+    def __init__(self, Layer, Num, bias):
         self.Layer = Layer
         self.Num = Num
         self.value = 0
+        self.bias = bias
 
 
 class connection:
-    def __init__(self, Layer, A, B, Init_Value, maxweight, bias):
+    def __init__(self, Layer, A, B, Init_Value, maxweight):
         self.Layer = Layer
         self.NodeA = A
         self.NodeB = B
         self.value = Init_Value
         self.weight = 1
         self.maxWeight = maxweight
-        self.bias = 0
 
     def send(self):
         self.NodeB.value = self.NodeA.value * self.value
@@ -44,37 +44,38 @@ class Handler:
 
         # Build the front Node Layer (input layer)
         for count in range(InitCount):
-            self.FrontLayerNodes.append(node(0, count))
+            self.FrontLayerNodes.append(node(0, count, 0))
 
         # Build the hidden Node Layers
         for x in range(self.HiddenLayerCount):
             temp = []
             for y in range(self.HiddenNodeCount):
-                temp.append(node(x, y))
+                temp.append(node((x+1), y, 0))
             self.HiddenLayerNodes.append(temp)
 
         # Build the back node Layer (output layer)
         for count in range(self.EndNodeCount):
-            self.BackLayerNodes.append(node(self.LayerCount - 1, count))
-
+            self.BackLayerNodes.append(node(self.LayerCount - 1, count, 0))
 
         startTime = time.time()
         self.createConnections()
         createTime = time.time() - startTime
         print(f'Network took {createTime} seconds to create')
+        print(len(self.connections))
 
     def Calculate(self, inputs: list):
-        tmp = 0
-        for x in self.connections:
-            for y in x:
-                tmp += len(y)
-        print(tmp)
+        if (len(inputs) == len(self.FrontLayerNodes)):
+            for count in range(len(self.FrontLayerNodes)):
+                self.FrontLayerNodes[count].value = inputs[count]
+                # put the input data into the front layer nodes
+            
+            for connectionLayer in self.connections:
+                for connectionNodeA in connectionLayer:
+                    for connectionNodeB in connectionNodeA:
+                        connectionNodeB.NodeB.value += (connectionNodeB.weight * connectionNodeB.NodeA.value)
 
-        # if (len(inputs) == len(self.FrontLayerNodes)):
-        #     for layer in self.connections:
-        #         self.CalcLayer(layer)
-        # else:
-        #     print(f'Input size of {len(inputs)} is not equal to the required {len(self.FrontLayerNodes)} needed.')
+        else:
+            print(f'Input size of {len(inputs)} is not equal to the required {len(self.FrontLayerNodes)} needed.')
 
     def CalcLayer(self, Layerconnections: list):
         for connection in Layerconnections:
@@ -86,7 +87,7 @@ class Handler:
 
     def connectTwoNodes(self, NodeA: node, NodeB: node, Init_Value=0.5, maxWeight=100):
         # This funky wunky function connects two Nodes by creating a connection object between the two
-        NewConnection = connection(NodeA.Layer, NodeA, NodeB, Init_Value, maxWeight, 0)
+        NewConnection = connection(NodeA.Layer, NodeA, NodeB, Init_Value, maxWeight)
         return NewConnection
 
     def connectNodeToNextLayer(self, NodeA: node):
@@ -107,25 +108,51 @@ class Handler:
         self.connections.append(tmp)
 
     def createConnections(self):
-        for layer in range(self.LayerCount - 2): # subtract 1 for indexing, one because there should be 1 less connection layer than node layers
+        for layer in range(self.LayerCount - 1): # subtract 1 for indexing, one because there should be 1 less connection layer than node layers
             self.connectLayerToNextLayer(layer)
 
     def getNodesinLayer(self, Layer):
         # Pretty basic explantion but this just returns all Nodes from a specific Layer
-        if (Layer == 0):
-            # returns front layer nodes
-            return self.FrontLayerNodes 
-        elif (Layer == (self.LayerCount - 1)):
-            # return back layer nodes
-            return self.BackLayerNodes
-        elif (Layer > 0 and Layer < (self.LayerCount - 1)):
-            # return Hidden layer nodes
-            return self.HiddenLayerNodes[Layer-2]
-        else:
-            # node Layer is non-existant, something has gone wrong
-            print(f'ERROR - Layer {Layer} is not a layer on this network - error occoured within function: getNodesinLayer')
-            return None
+        Nodes = []
+        try:
+            if (Layer == 0):
+                # returns front layer nodes
+                Nodes = self.FrontLayerNodes 
+            elif (Layer == (self.LayerCount - 1)):
+                # return back layer nodes
+                Nodes = self.BackLayerNodes
+            elif (Layer > 0 and Layer < (self.LayerCount)):
+                # return Hidden layer nodes
+                Nodes = self.HiddenLayerNodes[Layer - 1]
+            else:
+                # node Layer is non-existant, something has gone wrong
+                print(f'ERROR - Layer {Layer} is not a layer on this network - error occoured within function: getNodesinLayer')
+                Nodes = None
+            
+            # print(f"requested layer {Layer}, returned layer {Nodes[0].Layer}")
+            return Nodes
+        except Exception as e:
+            print(f"It brokey, Layer requested: {Layer}")
+            exit()
 
 
-Network = Handler(4, 784, 10, 16)
-Network.Calculate([0])
+Network = Handler(5, 784, 10, 16)
+testData  = list([0.1] * 784)
+Network.Calculate(testData)
+outputNodes = True
+if (outputNodes):
+    # Front layer:
+    print("Front layer")
+    for Node in Network.FrontLayerNodes:
+        print(Node.value)
+    # Hidden layers:
+    print("Hidden Layers")
+    for Layer in Network.HiddenLayerNodes:
+        for Node in Layer:
+            print(Node.value)
+    # back layer:
+    print("Back Layer")
+    for Node in Network.BackLayerNodes:
+        print(Node.value)
+
+del(Network)
