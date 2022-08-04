fc7fcb48695a867e09c79b9508c87cf55e16c7ec
AlfieRan <alfie.ranstead@outlook.com>
2022-08-04 14:47:02 +0100

    Created v2 using numpy and matricies and it works with a network [784,10,10]

---
+++ v1/imageparser.py
@@ -0,0 +1,23 @@
+from mnist import MNIST
+
+mndata = MNIST('../samples')
+
+
+def get_training():
+    images, labels = mndata.load_training()
+
+    allImages = mndata.process_images_to_lists(images)
+    resultList = [0] * 9
+
+    return [([v/255 for v in x], [1 if ri==labels[i] else 0 for ri,r in enumerate(resultList)]) for i,x in enumerate(allImages)]
+
+def get_testing():
+    images, labels = mndata.load_testing()
+    print(labels[0])
+    print(images[0])
+
+    allImages = mndata.process_images_to_lists(images)
+    resultList = [0] * 9
+
+    return [([v/255 for v in x], [1 if ri==labels[i] else 0 for ri,r in enumerate(resultList)]) for i,x in enumerate(allImages)]
+
---
+++ v2/main.py
@@ -0,0 +1,111 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import imageParser as ip
+import math
+
+X_TESTING, Y_TESTING = ip.get_training()
+X_TRAINING, Y_TRAINING = ip.get_testing()
+
+
+# currenctly called as init_params([784, 6, 6, 6, 10])
+def init_params(layers: list):
+    Weights = [np.random.randn(layers[i+1], layers[i]) for i in range(len(layers)-1)]
+    Biases = [np.random.randn(layers[i+1], 1) for i in range(len(layers)-1)]
+
+    return Weights, Biases
+
+def ReLU(inp):
+    return np.maximum(0, inp)
+
+def softmax(inp):
+    # print(f"Softmax input shape: {inp.shape}")
+    return np.exp(inp) / sum(np.exp(inp))
+
+def forward_prop(X, Weights, Biases):
+    Z = []
+    A = []
+    last = X
+
+    for i in range(len(Weights)):
+        # print(f"\n\nWeights {i} shape: {Weights[i].shape}\nBiases {i} shape: {Biases[i].shape}\n last shape: {last.shape}")
+        Z.append(Weights[i].dot(last) + Biases[i])
+        if (i == len(Weights) - 1):
+            # print(f"Layer: {i}, using softmax")
+            # print(Z[i].shape)
+            A.append(softmax(Z[i]))
+        else:
+            # print(f"Layer: {i}, using ReLU")
+            A.append(ReLU(Z[i]))
+
+        last = Z[i]
+
+    return A, Z
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def deriv_ReLU(inp):
+    return inp > 0
+
+def back_prop(A, Z, Weights, X, Y):
+    dZ = [0] * len(Weights) # derivative of Z
+    dW = [0] * len(Weights) # derivetive of weights
+    dB = [0] * len(Weights) # derivative of biases
+    one_hot_Y = one_hot(Y)
+    m = Y.size
+
+
+    # last layer
+    dZ[-1] = A[-1] - one_hot_Y # derivative of Z for the last layer
+    dW[-1] = 1 / m * dZ[-1].dot(A[-2].T) # derivative of W for the last layer
+    dB[-1] = 1 / m * np.sum(dZ[-1]) # derivative of B for the last layer
+
+    # other layers
+    for i in range(len(Weights)-2, -1, -1): # from the second last layer to the first, looping backwards
+        dZ[i] = Weights[i+1].T.dot(dZ[i+1]) * deriv_ReLU(Z[i]) # derivative of Z for the current layer
+
+        # Define whether to use the pervious layer's calculated weights or the input layer's weights
+        tmp = None
+        if len(A) + i: # if the layer previous to the current layer is the input layer
+            tmp = X     # input layer
+        else:
+            tmp = A[i-1] # previous layer
+
+        dW[i] = 1 / m * dZ[i].dot(tmp.T) # derivative of W for the current layer
+        dB[i] = 1 / m * np.sum(dZ[i]) # derivative of B for the current layer
+
+    return dW, dB
+
+def update_params(dW, dB, Weights, Biases, alpha):
+    for i in range(len(Weights)):
+        Weights[i] -= alpha * dW[i]
+        Biases[i] -= alpha * dB[i]
+
+    return Weights, Biases
+
+def get_predictions(A):
+    return np.argmax(A, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, iterations, alpha):
+    Weights, Biases = init_params([784, 10, 10])
+
+    for i in range(iterations):
+        A, Z = forward_prop(X, Weights, Biases)
+        dW, dB = back_prop(A, Z, Weights, X, Y)
+        Weights, Biases = update_params(dW, dB, Weights, Biases, alpha)
+
+        if (i % math.ceil(iterations / 10) == 0): # print every 1/10th of the iterations
+            predictions = get_predictions(A[-1])
+            print(f"Iteration: {i}\nAccuracy: {get_accuracy(predictions, Y)}")
+
+    return Weights, Biases
+
+
+Weights, Biases = gradient_descent(X_TRAINING, Y_TRAINING, 1000, 0.1)
